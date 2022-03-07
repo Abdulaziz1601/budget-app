3fb27da3ea105bb04606f71e8138ba2c93ea0258
Abdulaziz <abdulazizfaactory@gmail.com>
2022-03-07 16:11:21 +0500

    creating api for our app

---
+++ db.js
@@ -0,0 +1,22 @@
+const users = [{
+    id: 1,
+    mail: 'Abdulaziz',
+    password: 'k12jn31k2jn3',
+    age: 21,
+},
+{
+    id: 2,
+    mail: 'Ali',
+    password: 'asdadlskm',
+    age: 30,
+},
+{
+    id: 3,
+    mail: 'Halima',
+    password: 'halima',
+    age: 21,
+}];
+
+module.exports = {
+    users
+}
